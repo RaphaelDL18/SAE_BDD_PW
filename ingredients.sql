391f1c65de5fa7ef8c1f2e1f6afc560a61404fa5
MohamedBGTF <mohamed.bougatf@gmail.com>
2026-06-12 21:34:02 +0200

    Ajout insert ingrédients

---
+++ ingredients.sql
@@ -0,0 +1,81 @@
+-- =============================================
+-- INSERT Ingredients
+-- =============================================
+
+-- Légumes
+INSERT INTO Ingredient (nom) VALUES ('Tomate');
+INSERT INTO Ingredient (nom) VALUES ('Oignon');
+INSERT INTO Ingredient (nom) VALUES ('Ail');
+INSERT INTO Ingredient (nom) VALUES ('Carotte');
+INSERT INTO Ingredient (nom) VALUES ('Courgette');
+INSERT INTO Ingredient (nom) VALUES ('Poivron rouge');
+INSERT INTO Ingredient (nom) VALUES ('Poivron vert');
+INSERT INTO Ingredient (nom) VALUES ('Aubergine');
+INSERT INTO Ingredient (nom) VALUES ('Pomme de terre');
+INSERT INTO Ingredient (nom) VALUES ('Épinards');
+INSERT INTO Ingredient (nom) VALUES ('Brocoli');
+INSERT INTO Ingredient (nom) VALUES ('Champignon');
+INSERT INTO Ingredient (nom) VALUES ('Poireau');
+INSERT INTO Ingredient (nom) VALUES ('Céleri');
+INSERT INTO Ingredient (nom) VALUES ('Concombre');
+
+-- Viandes & Poissons
+INSERT INTO Ingredient (nom) VALUES ('Poulet');
+INSERT INTO Ingredient (nom) VALUES ('Boeuf haché');
+INSERT INTO Ingredient (nom) VALUES ('Lardons');
+INSERT INTO Ingredient (nom) VALUES ('Saumon');
+INSERT INTO Ingredient (nom) VALUES ('Thon en boîte');
+INSERT INTO Ingredient (nom) VALUES ('Crevettes');
+INSERT INTO Ingredient (nom) VALUES ('Escalope de dinde');
+INSERT INTO Ingredient (nom) VALUES ('Côte de porc');
+
+-- Produits laitiers
+INSERT INTO Ingredient (nom) VALUES ('Lait');
+INSERT INTO Ingredient (nom) VALUES ('Crème fraîche');
+INSERT INTO Ingredient (nom) VALUES ('Beurre');
+INSERT INTO Ingredient (nom) VALUES ('Fromage râpé');
+INSERT INTO Ingredient (nom) VALUES ('Parmesan');
+INSERT INTO Ingredient (nom) VALUES ('Mozzarella');
+INSERT INTO Ingredient (nom) VALUES ('Oeuf');
+INSERT INTO Ingredient (nom) VALUES ('Yaourt nature');
+
+-- Féculents & Céréales
+INSERT INTO Ingredient (nom) VALUES ('Pâtes');
+INSERT INTO Ingredient (nom) VALUES ('Riz');
+INSERT INTO Ingredient (nom) VALUES ('Farine');
+INSERT INTO Ingredient (nom) VALUES ('Pain de mie');
+INSERT INTO Ingredient (nom) VALUES ('Semoule');
+INSERT INTO Ingredient (nom) VALUES ('Lentilles');
+INSERT INTO Ingredient (nom) VALUES ('Pois chiches');
+INSERT INTO Ingredient (nom) VALUES ('Haricots rouges');
+
+-- Épices & Aromates
+INSERT INTO Ingredient (nom) VALUES ('Sel');
+INSERT INTO Ingredient (nom) VALUES ('Poivre');
+INSERT INTO Ingredient (nom) VALUES ('Cumin');
+INSERT INTO Ingredient (nom) VALUES ('Paprika');
+INSERT INTO Ingredient (nom) VALUES ('Curcuma');
+INSERT INTO Ingredient (nom) VALUES ('Cannelle');
+INSERT INTO Ingredient (nom) VALUES ('Basilic');
+INSERT INTO Ingredient (nom) VALUES ('Persil');
+INSERT INTO Ingredient (nom) VALUES ('Thym');
+INSERT INTO Ingredient (nom) VALUES ('Laurier');
+INSERT INTO Ingredient (nom) VALUES ('Coriandre');
+
+-- Sauces & Huiles
+INSERT INTO Ingredient (nom) VALUES ('Huile d''olive');
+INSERT INTO Ingredient (nom) VALUES ('Huile de tournesol');
+INSERT INTO Ingredient (nom) VALUES ('Sauce soja');
+INSERT INTO Ingredient (nom) VALUES ('Concentré de tomate');
+INSERT INTO Ingredient (nom) VALUES ('Moutarde');
+INSERT INTO Ingredient (nom) VALUES ('Vinaigre balsamique');
+INSERT INTO Ingredient (nom) VALUES ('Bouillon de poulet');
+
+-- Sucré & Pâtisserie
+INSERT INTO Ingredient (nom) VALUES ('Sucre');
+INSERT INTO Ingredient (nom) VALUES ('Sucre glace');
+INSERT INTO Ingredient (nom) VALUES ('Chocolat noir');
+INSERT INTO Ingredient (nom) VALUES ('Levure chimique');
+INSERT INTO Ingredient (nom) VALUES ('Vanille');
+INSERT INTO Ingredient (nom) VALUES ('Miel');
+INSERT INTO Ingredient (nom) VALUES ('Confiture');
